ef6ea1efaafe3f93a21b992ada8ceef6d20c9a72
Ag Stephens <ag.stephens@stfc.ac.uk>
2021-12-01 21:29:36 +0000

    Added: setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+VERSION = "2.0.0"
+
+setup(
+    name="AMF_CVs",
+    version=VERSION,
+    description="AMF controlled vocabularies",
+    include_package_data=True
+)
