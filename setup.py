ef6d645f6beae410c8bd02c2f5d40090d9691390
Ag Stephens <ag.stephens@stfc.ac.uk>
2021-12-02 07:54:58 +0000

    Updated: setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ VERSION = "2.0.0"
 
 setup(
     name="AMF_CVs",
+    author="Ag Stephens",
+    author_email="ag.stephens@stfc.ac.uk",
+    url="https://github.com/ncasuk/AMF_CVs",
     version=VERSION,
     description="AMF controlled vocabularies",
     package_data={'AMF_CVs': ['AMF_CVs/*', 'product-definitions/*', 'pyessv-vocabs/*']},
