98100f254eff1cee64b5c01c3869599d65071bfb
Ag Stephens <ag.stephens@stfc.ac.uk>
2021-12-02 10:22:39 +0000

    testing package data

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ setup(
     version=VERSION,
     description="AMF controlled vocabularies",
     packages=["test_setup"],
-    package_data={'test_setup': ['test_setup/hello.world']},
-    data_files=[('test_setup', ['test_setup/hello.world'])],
+#    package_data={'test_setup': ['test_setup/hello.world']},
+    data_files=[('amf-cvs', ['test_setup/hello.world'])],
 #    data_files=[
 #        ('AMF_CVs', collect_files('AMF_CVs', 'product-definitions', 'pyessv-vocabs'))
 #    ],
